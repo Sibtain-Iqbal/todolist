77c5d42d0aad49a4fea13a3fb748051c40496492
Sibtain Iqbal <139392522+Sibtain-Iqbal@users.noreply.github.com>
2024-02-04 01:09:40 -0800

    temp

--- src/data/java.js
+++ src/data/java.js
@@ -6,3 +6,11 @@ if (temperature > 30) {
 } else {
     console.log("It's not so hot outside.");
 }
+
+if (temperature > 30) {
+    console.log("It's hot outside!");
+} else if (temperature < 10) {
+    console.log("It's cold outside!");
+} else {
+    console.log("It's a pleasant day.");
+}
