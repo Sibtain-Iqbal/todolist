d667a1bf076e7501d1ce56dd9dcdee71b7581d5e
Sibtain Iqbal <139392522+Sibtain-Iqbal@users.noreply.github.com>
2024-02-05 01:11:05 -0800

    cases

--- src/data/java.js
+++ src/data/java.js
@@ -17,3 +17,28 @@ if (temperature > 30) {
 
 let message = temperature > 30 ? "It's hot outside!" : "It's not so hot.";
 console.log(message);
+
+switch (new Date().getDay()) {
+    case 0:
+        day = "Sunday";
+        break;
+    case 1:
+        day = "Monday";
+        break;
+    case 2:
+        day = "Tuesday";
+        break;
+    case 3:
+        day = "Wednesday";
+        break;
+    case 4:
+        day = "Thursday";
+        break;
+    case 5:
+        day = "Friday";
+        break;
+    case 6:
+        day = "Saturday";
+        break;
+}
+console.log("Today is " + day);
