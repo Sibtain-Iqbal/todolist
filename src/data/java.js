7f6054659e84697e17b8759bc94b17004ba9d66a
Sibtain Iqbal <139392522+Sibtain-Iqbal@users.noreply.github.com>
2024-02-06 01:11:43 -0800

    loop

--- src/data/java.js
+++ src/data/java.js
@@ -42,3 +42,13 @@ switch (new Date().getDay()) {
         break;
 }
 console.log("Today is " + day);
+
+
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+}
+let i = 0;
+while (i < 10) {
+    console.log(i);
+    i++;
+}
