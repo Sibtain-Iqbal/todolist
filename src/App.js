606878868c3dec34dedcdf2bab44b6c6ec15349e
Sibtain Iqbal <139392522+Sibtain-Iqbal@users.noreply.github.com>
2024-05-02 22:14:18 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
-import logo from './logo.svg';
+
 import './App.css';
 import { useState } from "react";
-import 'react-notifications/lib/notifications.css';
-import {NotificationContainer, NotificationManager} from 'react-notifications';
 function App() {
 
   const [todoList, settodoList] = useState([])
@@ -26,17 +24,12 @@ function App() {
 
   const items = todoList.map((value,index) =>{
 
-    let allValues= {
-      todoList,value,index,settodoList
-    }
     return(
       <TodoListItem values={value} key={index}
       indexnumber = {index}
       todoList={todoList}
       settodoList={settodoList}
-      
       />
-
     )
   })
 
@@ -69,15 +62,122 @@ function TodoListItem ({values,todoList,settodoList,indexnumber}){
  let [status,setstatus] = useState(false)
 
  let checkstatus =()=>{
-  setstatus(!status)
+  setstatus(!status)        
  }
 
 
   const deleterow = ()=> {
     let finals = todoList.filter((v,i)=>i!=indexnumber)
-    settodoList(finals)
+    settodoList(finals) 
   }
   return(
     <li className={(status) ? 'completetodo' : ''} onClick={checkstatus}>{indexnumber +1  }. {values } <span onClick={deleterow}>&times;</span></li>  
   )
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+import logo from './logo.svg';
+import './App.css';
+import { useState } from "react";
+
+
+
+
+
+function App() {
+
+  const [todolist,settodolist] = useState([])
+
+  let todosubmt = (event)=>{
+
+    let toname = event.target.toname.value
+    if(!todolist.includes(toname)){
+
+      let finaltodo = [toname, ...todolist]
+      settodolist(finaltodo)
+
+    }
+    else{
+      alert("todo tag is alredy saved")
+    }
+
+
+    const items = todolist.map((value,index) => {
+      return(
+        <Todoitems
+
+        value ={value}
+        index = {index}
+        todolist={todolist}
+        settodolist={settodolist}
+
+        
+        key={index}/>  
+        
+      )
+    })
+
+
+
+  
+
+
+
+   event.preventDefault()
+
+
+
+  }
+  return (
+    <div className="App">
+     <h1>Todo lsit</h1>
+     <form  onSubmit={todosubmt} >
+      <input  name = "toname"/>
+      <button>onSubmit</button>
+     </form>
+     
+     <div className='outerDiv'>
+      <ul>
+     {items}
+
+      </ul>
+
+     </div>
+
+     
+
+    </div>
+  );
+}
+
+export default App;
+
+
+
+function Todoitems({settodolist,todolist,value,indec
+}){
+
+  return(
+    <div>
+     <li>HTMM <span>&times;</span></li>
+    </div>
+  )
+
 }
