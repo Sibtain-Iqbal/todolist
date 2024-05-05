37582518879ba291f1a3c19919cda8f2fa1998e4
Sibtain Iqbal <139392522+Sibtain-Iqbal@users.noreply.github.com>
2024-05-05 08:19:28 -0700

    satte

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 
 import './App.css';
 import { useState } from "react";
+import tabs from './data/tabs'
 function App() {
 
   const [todoList, settodoList] = useState([])
@@ -9,7 +10,7 @@ function App() {
   const saveTodoList = (event) => {
     let toname = event.target.toname.value;
     if (!todoList.includes(toname)) {
-      let FinalTodo = [...todoList,toname]
+      let FinalTodo = [...todoList, toname]
       settodoList(FinalTodo)
 
     }
@@ -22,19 +23,24 @@ function App() {
     event.preventDefault()
   }
 
-  const items = todoList.map((value,index) =>{
+  const items = todoList.map((value, index) => {
 
-    return(
+    return (
       <TodoListItem values={value} key={index}
-      indexnumber = {index}
-      todoList={todoList}
-      settodoList={settodoList}
+        indexnumber={index}
+        todoList={todoList}
+        settodoList={settodoList}
       />
     )
   })
 
   return (
     <div className="App">
+      <div className='tabsouter'>
+        <h2 style={{ textAlign: 'left' }} >law prep vision mission and values</h2>
+
+      </div>
+
 
 
       <h1>Todo list</h1>
@@ -45,7 +51,7 @@ function App() {
       </form>
       <div className='outerDiv'>
         <ul>
-         {items}
+          {items}
         </ul>
       </div>
 
@@ -58,141 +64,35 @@ export default App;
 
 
 
-function TodoListItem ({values,todoList,settodoList,indexnumber}){
- let [status,setstatus] = useState(false)
+function TodoListItem({ values, todoList, settodoList, indexnumber }) {
+  let [status, setstatus] = useState(false)
 
- let checkstatus =()=>{
-  setstatus(!status)        
- }
-
-
-  const deleterow = ()=> {
-    let finals = todoList.filter((v,i)=>i!=indexnumber)
-    settodoList(finals) 
+  let checkstatus = () => {
+    setstatus(!status)
   }
-  return(
-    <li className={(status) ? 'completetodo' : ''} onClick={checkstatus}>{indexnumber +1  }. {values } <span onClick={deleterow}>&times;</span></li>  
-  )
-}
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-import logo from './logo.svg';
-import './App.css';
-import { useState } from "react";
-
-
-
-
-
-function App() {
-
-  const [todolist,settodolist] = useState([])
-
-  let todosubmt = (event)=>{
-
-    let toname = event.target.toname.value
-    if(!todolist.includes(toname)){
-
-      let finaltodo = [toname, ...todolist]
-      settodolist(finaltodo)
-
-    }
-    else{
-      alert("todo tag is alredy saved")
-    }
-
-
-    const items = todolist.map((value,index) => {
-      return(
-        <Todoitems
-
-        value ={value}
-        index = {index}
-        todolist={todolist}
-        settodolist={settodolist}
-
-        
-        key={index}/>  
-        
-      )
-    })
-
-    const itemss = todolist.map((value,index) => {
-      return(
-        <Todoitemms
-
-        value ={value}
-        index = {index}
-        todolist={todolist}
-        settodolist={settodolist}
-
-        
-        key={index}/>  
-        
-      )
-    })
-
-
-
-  
-
-
-
-   event.preventDefault()
-
-
-
+  const deleterow = () => {
+    let finals = todoList.filter((v, i) => i != indexnumber)
+    settodoList(finals)
   }
   return (
-    <div className="App">
-     <h1>Todo lsit</h1>
-     <form  onSubmit={todosubmt} >
-      <input  name = "toname"/>
-      <button>onSubmit</button>
-     </form>
-     
-     <div className='outerDiv'>
-      <ul>
-     {items}
-
-      </ul>
-
-     </div>
-
-     
-
-    </div>
-  );
-}
-
-export default App;
-
-
-
-function Todoitems({settodolist,todolist,value,indec
-}){
-
-  return(
-    <div>
-     <li>HTMM <span>&times;</span></li>
-    </div>
+    <li className={(status) ? 'completetodo' : ''} onClick={checkstatus}>{indexnumber + 1}. {values} <span onClick={deleterow}>&times;</span></li>
   )
-
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/data/tabs.jsx
@@ -0,0 +1,23 @@
+export const tasb = [
+    {
+      "title": "Object 1",
+      "description": "This is the description for the first object."
+    },
+    {
+      "title": "Object 2",
+      "description": "This is the description for the second object."
+    },
+    {
+      "title": "Object 3",
+      "description": "This is the description for the third object."
+    },
+    {
+      "title": "Object 4",
+      "description": "This is the description for the fourth object."
+    },
+    {
+      "title": "Object 5",
+      "description": "This is the description for the fifth object."
+    }
+  ]
+  
