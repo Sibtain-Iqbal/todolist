fb0000ecb2c7e0bf531e46a237193c6050788fb2
Sibtain Iqbal <139392522+Sibtain-Iqbal@users.noreply.github.com>
2024-05-06 10:07:14 -0700

    tabs

--- src/App.js
+++ src/App.js
@@ -8,6 +8,10 @@ function App() {
   const [todoList, settodoList] = useState([])
 
 
+
+
+
+
   const saveTodoList = (event) => {
     let toname = event.target.toname.value;
     if (!todoList.includes(toname)) {
@@ -35,35 +39,61 @@ function App() {
     )
   })
 
+
+
+
+
+
+
+
+
+
+
+  const [activetab, setactivetab] = useState(0)
+  const [tabshow, settabsshow] = useState(tabs[0])
+
+  const changebtncolor =(indexss)=>{
+    setactivetab(indexss)
+  }
+
+
   return (
     <div className="App">
-
-
-
-
-
-
-
-
-
-
-
+      
       <div className='tabsouter'>
         <h2 style={{ textAlign: 'left' }} >law prep vision mission and values</h2>
 
-      <ul>
-       {tabs.map((tabsitem,index)=> {
-        return(
-          <li>
-          <button>{tabsitem.title}</button>
-        </li>
+        <ul>
+          {tabs.map((tabsitem, indexss) => {
+            // alert(indexss)
+            return (
+              <li>
+                <button onClick={()=>changebtncolor()} className={activetab==indexss? 'activebutton' : ''}>{tabsitem.title}</button>
+              </li>
+            )
+          })}
 
-        )
-       })}
-       
-      </ul>
+        </ul>
+
+        {
+          tabshow !== undefined ?
+            <p>{tabshow.description}</p>
+            : ''
+        }
+
 
       </div>
+
+
+
+
+
+
+
+
+
+
+
 
 
 
